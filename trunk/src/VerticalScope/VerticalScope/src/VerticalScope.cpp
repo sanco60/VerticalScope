ce718beb63f46a31c3eee4a7226b4adf27f85253
sanco60 <sanco60@163.com>
2016-09-02 13:05:01 +0800

    modified:   trunk/src/VerticalScope/VerticalScope/src/VerticalScope.cpp

--- trunk/src/VerticalScope/VerticalScope/src/VerticalScope.cpp
+++ trunk/src/VerticalScope/VerticalScope/src/VerticalScope.cpp
@@ -2,9 +2,11 @@
 #include "plugin.h"
 
 #include "MemLeaker.h"
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <io.h>
 
-//#define MIN_SCALE 1
-//#define MAX_SCALE 10
 
 BOOL APIENTRY DllMain( HANDLE hModule, 
                        DWORD  ul_reason_for_call, 
@@ -34,27 +36,27 @@ void RegisterDataInterface(PDATAIOFUNC pfn)
 void GetCopyRightInfo(LPPLUGIN info)
 {
 	//填写基本信息
-	strcpy(info->Name,"最高最低空间");
-	strcpy(info->Dy,"US");
-	strcpy(info->Author,"john");
-	strcpy(info->Period,"短线");
-	strcpy(info->Descript,"最高最低空间");
-	strcpy(info->OtherInfo,"最高最低空间");
+	strcpy_s(info->Name,"最高最低空间");
+	strcpy_s(info->Dy,"US");
+	strcpy_s(info->Author,"john");
+	strcpy_s(info->Period,"短线");
+	strcpy_s(info->Descript,"最高最低空间");
+	strcpy_s(info->OtherInfo,"最高最低空间");
 
 	//填写参数信息
 	info->ParamNum = 3;//表示3个参数
 
-	strcpy(info->ParamInfo[0].acParaName,"百分比");
+	strcpy_s(info->ParamInfo[0].acParaName,"百分比");
 	info->ParamInfo[0].nMin=0;
 	info->ParamInfo[0].nMax=1000;
 	info->ParamInfo[0].nDefault=20;
 
-	strcpy(info->ParamInfo[1].acParaName,"高水位");
+	strcpy_s(info->ParamInfo[1].acParaName,"高水位");
 	info->ParamInfo[1].nMin=0;
 	info->ParamInfo[1].nMax=1;
 	info->ParamInfo[1].nDefault=1;
 
-	strcpy(info->ParamInfo[2].acParaName,"上涨");
+	strcpy_s(info->ParamInfo[2].acParaName,"上涨");
 	info->ParamInfo[2].nMin=0;
 	info->ParamInfo[2].nMax=1;
 	info->ParamInfo[2].nDefault=1;
@@ -66,7 +68,18 @@ void GetCopyRightInfo(LPPLUGIN info)
 const	BYTE	g_nAvoidMask[]={0xF8,0xF8,0xF8,0xF8};	// 无效数据标志(系统定义)
 
 char* g_nFatherCode[] = { "999999", "399001", "399005", "399006" };
+
 int g_FatherUpPercent[] = {-1, -1, -1, -1};
+
+const int cIgnoreStocksMaxCount = 5000;
+char g_IgnoreStocks[cIgnoreStocksMaxCount][7];
+
+bool g_bInitial = false;
+
+const char g_UserDir[] = {".\\UserData\\"};
+
+const char g_IgnoreKeyword[] = {"IGS_VerticalScope*.EBK"};
+
 
 typedef enum _eFatherCode
 {
@@ -133,10 +146,10 @@ LPHISDAT minClose(LPHISDAT pHisDat, long lDataNum)
 	return pMin;
 }
 
-BOOL fEqual(float a, float b)
+BOOL fEqual(double a, double b)
 {
-	const float cJudge = 0.001;
-	float fValue = 0.0;
+	const double cJudge = 0.001;
+	double fValue = 0.0;
 
 	fValue = (a > b) ? a - b : b - a;
 
@@ -222,6 +235,21 @@ BOOL filterStock(char * Code, short nSetCode, NTime time1, NTime time2, BYTE nTQ
 	if (NULL == Code)
 		return FALSE;
 
+	{
+		//用户指定的股票需要屏蔽
+		for (int iRow = 0; iRow < cIgnoreStocksMaxCount; iRow++)
+		{
+			if (0 == strlen(g_IgnoreStocks[iRow]))
+				break;
+
+			if (0 == strcmp(g_IgnoreStocks[iRow], Code))
+			{
+				OutputDebugStringA("User abandon ");
+				return FALSE;
+			}
+		}
+	}
+
 	const unsigned short cMinYears = 2;	
 	const short cInfoNum = 2;
 	short iInfoNum = cInfoNum;
@@ -253,11 +281,11 @@ BOOL filterStock(char * Code, short nSetCode, NTime time1, NTime time2, BYTE nTQ
 		memset(&dInterval, 0, sizeof(NTime));
 
 		long lStartDate = pStockInfo->J_start;
-		startDate.year = lStartDate / 10000;
+		startDate.year = (short)(lStartDate / 10000);
 		lStartDate = lStartDate % 10000;
-		startDate.month = lStartDate / 100;
+		startDate.month = (unsigned char)(lStartDate / 100);
 		lStartDate = lStartDate % 100;
-		startDate.day = lStartDate;
+		startDate.day = (unsigned char)lStartDate;
 
 		//获取今天日期
 		SYSTEMTIME tdTime;
@@ -265,15 +293,15 @@ BOOL filterStock(char * Code, short nSetCode, NTime time1, NTime time2, BYTE nTQ
 		GetLocalTime(&tdTime);
 
 		todayDate.year = tdTime.wYear;
-		todayDate.month = tdTime.wMonth;
-		todayDate.day = tdTime.wDay;
+		todayDate.month = (unsigned char)tdTime.wMonth;
+		todayDate.day = (unsigned char)tdTime.wDay;
 
 		dInterval = dateInterval(startDate, todayDate);
 
 		//太年轻的股返回FALSE
 		if (dInterval.year < cMinYears)
 		{
-			//delete[] pStockInfo;
+			OutputDebugStringA("It's too young.");
 			pStockInfo = NULL;
 			return FALSE;
 		}
@@ -281,7 +309,7 @@ BOOL filterStock(char * Code, short nSetCode, NTime time1, NTime time2, BYTE nTQ
 		//delete[] pStockInfo;
 		pStockInfo = NULL;
 	}
-	
+	/*
 	{
 		//获取股票当天的信息，当天停牌的返回FALSE
 		REPORTDAT2 reportArray[cInfoNum];
@@ -303,7 +331,7 @@ BOOL filterStock(char * Code, short nSetCode, NTime time1, NTime time2, BYTE nTQ
 			return FALSE;
 		}
 		pReportDat2 = NULL;
-	}	
+	}*/	
 
 	return TRUE;
 }
@@ -325,7 +353,7 @@ int calcMax2MinPercent(char * Code, short nSetCode, short DataType, NTime time1,
 
 	LPHISDAT pHisDat = new HISDAT[datanum];
 
-	long readnum = g_pFuncCallBack(Code, nSetCode, DataType, pHisDat, datanum, time1, time2, nTQ, 0);
+	long readnum = g_pFuncCallBack(Code, nSetCode, DataType, pHisDat, (short)datanum, time1, time2, nTQ, 0);
 	if ( 1 > readnum || readnum > datanum )
 	{
 		OutputDebugStringA("========= g_pFuncCallBack read error! =========\n");
@@ -333,18 +361,6 @@ int calcMax2MinPercent(char * Code, short nSetCode, short DataType, NTime time1,
 		pHisDat = NULL;
 		return iMax2Min;
 	}
-
-	//停牌股不计算直接返回
-	/*LPHISDAT pLate = pHisDat + readnum - 1;
-	if (FALSE == dateEqual(pLate->Time, time2)
-		|| fEqual(pLate->fVolume, 0.0))
-	{
-		OutputDebugStringA(Code);
-		OutputDebugStringA("====== Stop trading today. \n");
-		delete[] pHisDat;
-		pHisDat = NULL;
-		return iMax2Min;
-	}*/
 
 	//查找最高收盘价
 	pMax = maxClose(pHisDat, readnum);
@@ -377,6 +393,63 @@ int calcMax2MinPercent(char * Code, short nSetCode, short DataType, NTime time1,
 }
 
 
+void restoreIgnoreStocks()
+{
+	long lFind = 0;
+	struct _finddata_t fInfo;
+	std::string szFind = g_UserDir;
+	szFind.append(g_IgnoreKeyword);
+
+	memset(g_IgnoreStocks, 0, sizeof(g_IgnoreStocks));
+
+	lFind = _findfirst(szFind.c_str(), &fInfo);
+	if (-1 == lFind)
+		return;
+
+	int iRow = 0;
+	do{
+		std::string szFilePath = g_UserDir;
+		szFilePath.append(fInfo.name);
+		
+		std::ifstream ifs;
+		ifs.open(szFilePath);
+		if (!ifs.is_open())
+			continue;
+
+		while(!ifs.eof())
+		{
+			char szLine[64] = {0};
+			ifs.getline(szLine, 63);
+			std::string dataLine(szLine+1);
+			if (6 == dataLine.length())
+			{
+				if (iRow >= cIgnoreStocksMaxCount)
+					break;
+				strcpy_s(g_IgnoreStocks[iRow++], dataLine.c_str());
+			}
+		}
+		ifs.close();
+		
+	} while (0 == _findnext(lFind, &fInfo));
+	_findclose(lFind);
+
+	return;
+}
+
+
+bool init()
+{
+	if (g_bInitial)
+		return false;
+
+	restoreIgnoreStocks();
+
+	g_bInitial = true;
+
+	return true;
+}
+
+
 BOOL InputInfoThenCalc1(char * Code,short nSetCode,int Value[4],short DataType,short nDataNum,BYTE nTQ,unsigned long unused) //按最近数据计算
 {
 	BOOL nRet = FALSE;
@@ -391,37 +464,15 @@ BOOL InputInfoThenCalc2(char * Code,short nSetCode,int Value[4],short DataType,N
 		|| (Value[1] != 0 && Value[1] != 1) 
 		|| (Value[2] != 0 && Value[2] != 1) 
 		|| NULL == Code )
+	{
+		OutputDebugStringA("Parameters Error!\n");
 		goto endCalc2;
+	}
 
-	//int iFatherRate = 0;
+	if (!g_bInitial)
+		init();
+
 	int iSonRate = 0;
-
-	/* 计算对应大盘垂直空间百分比 */
-	/*EFatherCode eFCode = mathFatherCode(Code);
-	if (EFatherCodeMax == eFCode)
-	{
-		OutputDebugStringA("========= Didn't find FatherCode for ");
-		OutputDebugStringA(Code);
-		OutputDebugStringA(" =========\n");
-		goto endCalc2;
-	}
-	//判断是否计算过对应指数
-	if (g_FatherUpPercent[eFCode] < 0)
-	{
-		//计算对应指数百分比
-		iFatherRate = calcMax2MinPercent(g_nFatherCode[eFCode], nSetCode, DataType, time1, time2, nTQ);
-		if ( iFatherRate < 0 )
-		{
-			OutputDebugStringA("=========== father calcMax2MinPercent error!!\n");
-			goto endCalc2;
-		}
-		g_FatherUpPercent[eFCode] = iFatherRate;
-	}
-	else
-	{
-		//读取已计算过的数据
-		iFatherRate = g_FatherUpPercent[eFCode];
-	}	*/
 
 	/* 过滤垃圾股和停牌股 */
 	if (FALSE == filterStock(Code, nSetCode, time1, time2, nTQ))
@@ -440,12 +491,6 @@ BOOL InputInfoThenCalc2(char * Code,short nSetCode,int Value[4],short DataType,N
 		goto endCalc2;
 	}
 	
-	/*int ionecopy = ifatherrate/value[0];
-	ionecopy = (0 == ionecopy) ? 1 : ionecopy;
-
-	int iline = ifatherrate + value[1]*ionecopy;
-	iline = (0 > iline) ? 0 : iline;*/
-
 	if (0 == Value[1])
 	{
 		if (iSonRate <= Value[0])
